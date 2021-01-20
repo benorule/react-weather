2115cc061f60138eb51d5145b5c528d3d958aa81
unknown <benorule@outlook.com>
2021-01-20 18:31:13 +1030

    add loading icon and error message for invalid city name input

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -3,16 +3,18 @@ import '../../App.css';
 import './Conditions.css';
 
 const conditions = (props) => {
-   return (
-       <div>
-           {props.responseObj.cod === 200 ?
-               <div>
-                   <p className="text">In {props.responseObj.name} it is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
-               </div>
-           : null
-           }
-       </div>
-   )
+    return (
+        <div>
+            {props.error && <div className="text">Please enter a valid city.</div>}
+            {props.loading && <div className="text">Loading...</div>}
+            {props.responseObj.cod === 200 ?
+                <div>
+                    <p className="text">In {props.responseObj.name} it is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                </div>
+                : null
+            }
+        </div>
+    )
 }
 
 export default conditions;
