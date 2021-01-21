d30e35c8fd5226c831d8776314f89b2abbdb761f
unknown <benorule@outlook.com>
2021-01-21 18:06:33 +1030

    Updates

--- precache-manifest.64c97e05eaac22695c73250dd2170fc6.js
+++ precache-manifest.64c97e05eaac22695c73250dd2170fc6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd39364631c88c8fde07e2863c7a6080",
+    "revision": "e2085c96754add12012fbb7fc6b4fe3f",
     "url": "/react-weather/index.html"
   },
   {
-    "revision": "c77fd5bd58ec66ef40bf",
+    "revision": "d79050600b7baa162a23",
     "url": "/react-weather/static/css/main.ff667bac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-weather/static/js/2.e41113a8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c77fd5bd58ec66ef40bf",
-    "url": "/react-weather/static/js/main.6323ac01.chunk.js"
+    "revision": "d79050600b7baa162a23",
+    "url": "/react-weather/static/js/main.e2a72138.chunk.js"
   },
   {
     "revision": "e5c44b0578b08a2ae962",
