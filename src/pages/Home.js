27202a3b2875475f11456c6048071a31bd994e40
unknown <benorule@outlook.com>
2021-01-21 21:36:08 +1030

    removed footer component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../App.css';
 import HeroSection from '../components/HeroSection/HeroSection.js';
-import Footer from '../components/Footer/Footer.js';
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Footer />
     </>
   );
 }
